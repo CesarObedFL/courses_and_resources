403262dd717e2fad53cde8cb676ea77d882bea0d
CesarObedFL <cesarobedfl@gmail.com>
2023-06-26 15:32:53 -0600

    [START] new php course

---
+++ courses/php/blog_project/index.php
@@ -0,0 +1,18 @@
+<DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog de Videojuegos</title>
+    </head>
+
+    <body>
+        <!-- header -->
+        <!-- /. header -->
+
+        <!-- menu -->
+        <!-- /. menu -->
+
+        <!-- sidebar -->
+        <!-- /. sidebar -->
+    </body>
+</html>
