1601bcf65401ccafdc0cf884fb8a98946fafc275
CesarObedFL <cesarobedfl@gmail.com>
2023-06-26 15:43:13 -0600

    [ADD] nav menu

--- courses/php/blog_project/index.php
+++ courses/php/blog_project/index.php
@@ -2,17 +2,38 @@
 <html lang="es">
     <head>
         <meta charset="utf-8">
-        <title>Blog de Videojuegos</title>
+        <title>Blog de VideoJuegos</title>
     </head>
 
     <body>
         <!-- header -->
-        <!-- /. header -->
+        <header>
+            <div id="logo">
+                <a href="index.php">
+                    Blog de VideoJuegos
+                </a>
+            </div>
 
-        <!-- menu -->
-        <!-- /. menu -->
+            <!-- menu -->
+            <nav id="nav">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="index.php">Categoria 1</a></li>
+                    <li><a href="index.php">Categoria 2</a></li>
+                    <li><a href="index.php">Categoria 3</a></li>
+                    <li><a href="index.php">Categoria 4</a></li>
+                    <li><a href="index.php">Sobre mí</a></li>
+                    <li><a href="index.php">Contacto</a></li>
+                </ul>
+            </nav>
+            <!-- /. menu -->
+        </header>
+        <!-- /. header -->
 
         <!-- sidebar -->
         <!-- /. sidebar -->
+
+        <!-- main -->
+        <!-- /. main -->
     </body>
 </html>
