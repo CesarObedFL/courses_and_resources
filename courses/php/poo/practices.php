4a14df4000083fc2fef0d440118d406808a85ec8
CesarObedFL <cesarobedfl@gmail.com>
2023-07-18 09:05:50 -0600

    [ADD] passing variables to functions by reference

--- courses/php/poo/practices.php
+++ courses/php/poo/practices.php
@@ -26,6 +26,21 @@ echo $pi * ($radio * $radio);
 $b = 0;
 $b ? print 'true' : print 'false';
 
+// passing variables to functions by reference
+$a1 = "WILLIAM";
+$a2 = "henry";
+$a3 = "gatES";
+
+echo $a1 . " " . $a2 . " " . $a3;
+fix_names($a1, $a2, $a3);
+echo $a1 . " " . $a2 . " " . $a3;
+
+function fix_names(&$n1, &$n2, &$n3)
+{
+    $n1 = ucfirst(strtolower($n1));
+    $n2 = ucfirst(strtolower($n2));
+    $n3 = ucfirst(strtolower($n3));
+}
 
 
 ?>
