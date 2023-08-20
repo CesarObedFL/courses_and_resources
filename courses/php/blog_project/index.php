dd779f9c40946eb8c9cf205262075551d3859e55
CesarObedFL <cesarobedfl@gmail.com>
2023-08-20 14:55:57 -0600

    [ADD] aside_bar.php file

--- courses/php/blog_project/index.php
+++ courses/php/blog_project/index.php
@@ -2,36 +2,8 @@
 
 
         <div id="container">
-            <!-- sidebar -->
-            <aside id="sidebar">
-                <div id="login" class="block-aside">
-                    <h3>Identificate</h3>
-                    <form action="login.php" method="POST">
-                        <label for="email">Email</label>
-                        <input type="email" name="email">
-                        <label for="password">Password</label>
-                        <input type="password" name="password">
-
-                        <input type="submit" value="ENTRAR">
-                    </form>
-                </div>
-
-                <div id="register" class="block-aside">
-                    <h3>Regístrate</h3>
-                    <form action="register.php" method="POST">
-                        <label for="name">Nombre</label>
-                        <input type="email" name="email">
-                        <label for="email">Email</label>
-                        <input type="email" name="email">
-                        <label for="password">Password</label>
-                        <input type="password" name="password">
-
-                        <input type="submit" value="REGISTRARSE">
-                    </form>
-                </div>
-
-            </aside>
-            <!-- /. sidebar -->
+            
+            <?php require_once 'includes/aside_bar.php'; ?>
 
             <!-- main -->
             <div id="main">
