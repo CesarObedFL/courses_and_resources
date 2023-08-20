4916a1f924798bfcb7296b2175b29dded65db687
CesarObedFL <cesarobedfl@gmail.com>
2023-08-20 14:51:34 -0600

    [FIX] database_connection.php file

--- courses/php/blog_project/includes/database_connection.php
+++ courses/php/blog_project/includes/database_connection.php
@@ -4,6 +4,6 @@ $server = 'localhost';
 $username = 'root';
 $password = '';
 $database = 'blog_project_db';
-$db = mysql_connect($server, $username, $password, $database);
+$db = mysqli_connect($server, $username, $password, $database);
 
-mysql_query($db, "SET NAMES 'utf-8'");
+mysqli_query($db, "SET NAMES 'utf-8'");
--- courses/php/blog_project/index.php
+++ courses/php/blog_project/index.php
@@ -1,36 +1,5 @@
-<DOCTYPE HTML>
-<html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog de VideoJuegos</title>
-        <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
-    </head>
+<?php require_once 'includes/header.php'; ?>
 
-    <body>
-        <!-- header -->
-        <header id="header">
-            <div id="logo">
-                <a href="index.php">
-                    Blog de VideoJuegos
-                </a>
-            </div>
-
-            <!-- menu -->
-            <nav id="nav-menu">
-                <ul>
-                    <li><a href="index.php">Inicio</a></li>
-                    <li><a href="index.php">Categoria 1</a></li>
-                    <li><a href="index.php">Categoria 2</a></li>
-                    <li><a href="index.php">Categoria 3</a></li>
-                    <li><a href="index.php">Categoria 4</a></li>
-                    <li><a href="index.php">Sobre mí</a></li>
-                    <li><a href="index.php">Contacto</a></li>
-                </ul>
-            </nav>
-            <!-- /. menu -->
-            <div class="clearfix"></div>
-        </header>
-        <!-- /. header -->
 
         <div id="container">
             <!-- sidebar -->
