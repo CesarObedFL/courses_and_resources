1196a3c8c4fdfbf028f772ce80a4144d0a5feff7
CesarObedFL <cesarobedfl@gmail.com>
2023-09-29 11:03:35 -0600

    [ADD] update using unique email user

--- courses/php/blog_project/actions/update_profile.php
+++ courses/php/blog_project/actions/update_profile.php
@@ -20,6 +20,7 @@ if ( isset($_POST ) ) {
 
     if ( !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL) ) {
         $email_validated = true; 
+
     } else {
         $email_validated = false;
         $errors['email'] = "variable email invalid!...";
@@ -32,18 +33,28 @@ if ( isset($_POST ) ) {
     if ( count($errors) == 0 ) {
         $save_data = true;
         $user = $_SESSION['user'];
-        $sql = "UPDATE users SET name = '$name', email = '$email' WHERE id = " . $user['id'];
-        $result = mysqli_query($db, $sql);
 
-        if ($result) {
-            $_SESSION['user']['name'] = $name;
-            $_SESSION['user']['email'] = $email;
-            $_SESSION['saved'] = "User updated successfully!...";
+        $sql = "SELECT id, email FROM users WHERE email = '$email'";
+        $isset_email = mysqli_query($db, $sql);
+        $isset_user = mysqli_fetch_assoc($isset_email);
+
+        if ( $isset_user['id'] == $user['id'] || empty($isset_user) ) {
+
+            
+            $sql = "UPDATE users SET name = '$name', email = '$email' WHERE id = " . $user['id'];
+            $result = mysqli_query($db, $sql);
+
+            if ($result) {
+                $_SESSION['user']['name'] = $name;
+                $_SESSION['user']['email'] = $email;
+                $_SESSION['saved'] = "User updated successfully!...";
+            } else {
+                $_SESSION['errors']['saved'] = "Some error has happened until updating user!...";
+            }
+
         } else {
-            $_SESSION['errors']['saved'] = "Some error has happened until updating user!...";
+            $_SESSION['errors']['saved'] = "email used already, you can't update it!...";
         }
-
-        
 
     } else {
         $_SESSION['errors'] = $errors;
