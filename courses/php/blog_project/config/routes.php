5b49ebe95e0de3b4f82d5685c2a8e6b6dfa47234
CesarObedFL <cesarobedfl@gmail.com>
2023-10-02 10:26:00 -0600

    [ADD] config/routes file

---
+++ courses/php/blog_project/config/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].'/courses_and_resources/courses/php/blog_project/');
+define('CONFIG_PATH', ROOT_PATH.'config/');
+define('ACTIONS_PATH', ROOT_PATH.'actions/');
+define('ASSETS_PATH', ROOT_PATH.'assets/');
+define('INCLUDES_PATH', ROOT_PATH.'includes/');
+define('VIEWS_PATH', ROOT_PATH.'views/');
