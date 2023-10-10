a1a17ef34268ab8c7b852c8317460a5c4d70f0d8
CesarObedFL <cesarobedfl@gmail.com>
2023-10-10 10:00:46 -0600

    [FIX] searching functionallity

--- courses/php/blog_project/views/searching.php
+++ courses/php/blog_project/views/searching.php
@@ -4,35 +4,34 @@
     
 <?php require_once INCLUDES_PATH.'/aside_bar.php'; ?>
 
-<?php
-    if ( !isset($_POST['search']) ) {
-        header('Location: ../index.php');
-    } 
-?>
-
 <!-- main -->
 <div id="main">
 
     <?php 
-        $articles = get_articles($db, null, null, $_POST['search']);
-        if ( isset($articles) && !empty($articles) ):
-    ?>
-        <h1>Artículos encontrados con : <?=$_POST['search']?></h1>
-        <?php 
-            while ( $article = mysqli_fetch_assoc($articles) ): ?>
-                <article class="article">
-                    <a href="<?=VIEWS_PATH.'/show_article_by_id.php?id='.$article['id']?>"><h2 class="article-title"><?=$article['title']?></h2></a>
-                    <span class="b-date"><?=$article['category'].' | '.$article['date']?></span>
-                    <p>
-                        <?=substr($article['description'],0, 160) . '...'?>
-                    </p>
-                </article>
-            <?php endwhile; ?>
+        if ( isset($_POST['search']) && !empty($_POST['search']) ):
+            $articles = get_articles($db, null, null, $_POST['search']);
+            if ( isset($articles) && !empty($articles) ):
+        ?>
+            <h1>Artículos encontrados con : <?=$_POST['search']?></h1>
+            <?php 
+                while ( $article = mysqli_fetch_assoc($articles) ): ?>
+                    <article class="article">
+                        <a href="<?=VIEWS_PATH.'/show_article_by_id.php?id='.$article['id']?>"><h2 class="article-title"><?=$article['title']?></h2></a>
+                        <span class="b-date"><?=$article['category'].' | '.$article['date']?></span>
+                        <p>
+                            <?=substr($article['description'],0, 160) . '...'?>
+                        </p>
+                    </article>
+                <?php endwhile; ?>
+        <?php else: ?>
+            <h1> No se encontró ningún artículo con <?=$_POST['search']?>!... </h1>
+        <?php endif; ?>
     <?php else: ?>
-        <h1> No se encontró ningún artículo con <?=$_POST['search']?>!... </h1>
-    <?php endif; ?>
+        <h1> Error al buscar!... </h1>
+    <?php endif;?>
 
 </div>
 <!-- /. main -->
             
 <?php require_once INCLUDES_PATH.'/footer.php'; ?>
+
