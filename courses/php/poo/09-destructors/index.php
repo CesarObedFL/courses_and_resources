3f00193de2f331113d1a4d3f8952348eadbc5b93
CesarObedFL <cesarobedfl@gmail.com>
2023-10-16 08:45:18 -0600

    [FIX] destruct function

--- courses/php/poo/09-destructors/index.php
+++ courses/php/poo/09-destructors/index.php
@@ -8,12 +8,14 @@ class User {
     {
         $this->name = "Cesar Obed";
         $this->email = "cesarobedfl@gmail.com";
+        echo "creating the object!...";
     }
 
-    public function __desctruct()
+    public function __destruct()
     {
         $this->name = "";
         $this->email = "";
+        echo 'destroying the object!...';
     }
 }
 
