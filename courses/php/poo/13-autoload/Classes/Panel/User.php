2c0aeb1e38e9c391b1f72591a009ee89bba57042
CesarObedFL <cesarobedfl@gmail.com>
2023-10-19 09:28:51 -0600

    [ADD] classes and methos exists lesson

--- courses/php/poo/13-autoload/Classes/Panel/User.php
+++ courses/php/poo/13-autoload/Classes/Panel/User.php
@@ -3,8 +3,8 @@
 namespace Panel;
 
 class User {
-    public $name;
-    public $email;
+    private $name;
+    private $email;
 
     public function __construct()
     {
@@ -15,5 +15,32 @@ class User {
     public function __toString()
     {
         return $this->name . " : " . $this->email;
+    }
+
+    public function __call($name, $arguments)
+    {
+        $prefix_method = substr($name, 0, 3);
+        
+        if ( $prefix_method == 'get' ) {
+            $method_name = substr(strtolower($name), 3);
+
+            if ( isset($this->$method_name) ) {
+                return $this->$method_name;
+            } else {
+                return "Inexistent Method!...";
+            }
+        } else if ( $prefix_method = 'set' ) {
+            $method_name = substr(strtolower($name), 3);
+            
+            if ( isset($this->$method_name) ) {
+                $this->$method_name = $arguments[0];
+                //var_dump($arguments);
+            } else {
+                return "Inexistent Method!...";
+            }
+            
+        } else {
+            return "Inexistent Method!...";
+        }
     }
 }
--- courses/php/poo/13-autoload/index.php
+++ courses/php/poo/13-autoload/index.php
@@ -43,3 +43,54 @@ echo "<br><hr><br>";
 
 $user = new UserAdmin();
 var_dump($user);
+
+
+echo "<br><hr><h1>Búsqueda de Clases</h1><br>";
+
+// búsqueda de clases
+/**
+ * El símbolo @ en PHP se utiliza para silenciar los errores y advertencias que pueden generarse al ejecutar una función o expresión. 
+ * Cuando se coloca @ delante de una función o expresión, PHP no mostrará ningún mensaje de error o advertencia que se genere 
+ * durante la ejecución de esa función o expresión. 
+ * En otras palabras, suprime la notificación de errores.
+ * 
+ * Esto puede ser útil en situaciones en las que se espera que una función genere errores, pero no se desean mostrar en la salida 
+ * al usuario o en los registros del servidor. Sin embargo, el uso excesivo de @ para suprimir errores no se recomienda, 
+ * ya que puede hacer que sea difícil depurar problemas en el código y ocultar problemas subyacentes.
+ */
+
+ if ( @class_exists('User') ) {
+    echo "<h2>class User exist!...</h2>";
+} else {
+    echo "<h2>class User don't exist!...</h2>";
+}
+
+if ( @class_exists('Models\User') ) {
+    echo "<h2>class Models\User exist!...</h2>";
+} else {
+    echo "<h2>class Models\User don't exist!...</h2>";
+}
+
+echo "<br><hr><h1>Búsqueda de Métodos</h1><br>";
+
+// búsqueda de métodos de clase
+
+var_dump(get_class_methods($user));
+
+$methods = get_class_methods($user);
+$search_method = array_search('set_name', $methods);
+
+if ( $search_method ) {
+    echo "<h2>method set_name exist on: " . $search_method . "</h2><br>";
+} else {
+    echo "<h2>method set_name don't exist!...</h2><br>";
+}
+
+$search_method = array_search('__call', $methods);
+
+if ( $search_method ) {
+    echo "<h2>method __call exist on: " . $search_method . " position</h2><br>";
+} else {
+    echo "<h2>method __call don't exist!...</h2><br>";
+}
+
