3bd2042c122c6b0003b411d8f212dbdeefae9c92
CesarObedFL <cesarobedfl@gmail.com>
2023-10-30 09:29:24 -0600

    [ADD] database config

--- courses/php/mvc/config/database.php
+++ courses/php/mvc/config/database.php
@@ -2,9 +2,14 @@
 
 class Database {
 
+    private $server = 'cyr_database_container';
+    private $username = 'root'; // using root for user permissions
+    private $password = 'secret';
+    private $database = 'cyr_db';
+
     public static function connect()
     {
-        $connection = new mysqli("localhost", "user", "password", "database");
+        $connection = new mysqli($this->server, $this->username, $this->password, $this->database);
 
         $connection->query("SET NAMES 'utf8'");
 
