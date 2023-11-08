f26ac7e626bf0de96c6ae46e75e4dfef42c2cec0
CesarObedFL <cesarobedfl@gmail.com>
2023-11-08 13:28:53 -0600

    [ADD] header styles

--- courses/php/webstore_project/index.php
+++ courses/php/webstore_project/index.php
@@ -9,82 +9,82 @@
     </head>
 
     <body>
+        <div id="container">
+            <!-- header -->
+            <header id="header">
+                <div id="logo">
+                    <img src="assets/img/logo.png" alt="logo">
+                    <a href="index.php">
+                        clothes store
+                    </a>
+                </div>
+            </header>
+            <!-- /. header -->
 
-        <!-- header -->
-        <header id="header">
-            <div id="logo">
-                <img src="assets/img/logo.png" alt="logo">
-                <a href="index.php">
-                    clothes store
-                </a>
+            <!-- menu -->
+            <nav id="menu">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Categoria 1</a></li>
+                    <li><a href="#">Categoria 2</a></li>
+                    <li><a href="#">Categoria 3</a></li>
+                    <li><a href="#">Categoria 4</a></li>
+                    <li><a href="#">Categoria 5</a></li>
+                </ul>
+            </nav>
+            <!-- /. menu -->
+
+            <div id="content">
+                <!-- sidebar -->
+                <aside id="aside">
+                    <div id="login" class="block-aside">
+                        <form action="#" method="POST">
+                            <label for="email">Email</label>
+                            <input type="email" name="email">
+                            <label for="password">Password</label>
+                            <input type="password" name="password">
+                            <input type="submit" value="entrar">
+                        </form>
+
+                        <a href="#">My Orders</a>
+                        <a href="#">My Profile</a>
+                    </div>
+                </aside>
+                <!-- /. sidebar -->
+
+                <!-- main -->
+                <div id="main">
+                    <div class="product">
+                        <img src="assets/img/blue-shirt.png" alt="blue shirt">
+                        <h2>Blue Shirt</h2>
+                        <p>$20</p>
+                        <a href="#">Buy</a>
+                    </div>
+
+                    <div class="product">
+                        <img src="assets/img/red-shirt.png" alt="blue shirt">
+                        <h2>Red Shirt</h2>
+                        <p>$20</p>
+                        <a href="#">Buy</a>
+                    </div>
+
+                    <div class="product">
+                        <img src="assets/img/green-shirt.png" alt="blue shirt">
+                        <h2>Green Shirt</h2>
+                        <p>$20</p>
+                        <a href="#">Buy</a>
+                    </div>
+
+                </div>
+                <!-- /. main -->
             </div>
-        </header>
-        <!-- /. header -->
 
-        <!-- menu -->
-        <nav id="menu">
-            <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Categoria 1</a></li>
-                <li><a href="#">Categoria 2</a></li>
-                <li><a href="#">Categoria 3</a></li>
-                <li><a href="#">Categoria 4</a></li>
-                <li><a href="#">Categoria 5</a></li>
-            </ul>
-        </nav>
-        <!-- /. menu -->
-
-        <div id="content">
-            <!-- sidebar -->
-            <aside id="aside">
-                <div id="login" class="block-aside">
-                    <form action="#" method="POST">
-                        <label for="email">Email</label>
-                        <input type="email" name="email">
-                        <label for="password">Password</label>
-                        <input type="password" name="password">
-                        <input type="submit" value="entrar">
-                    </form>
-
-                    <a href="#">My Orders</a>
-                    <a href="#">My Profile</a>
-                </div>
-            </aside>
-            <!-- /. sidebar -->
-
-            <!-- main -->
-            <div id="main">
-                <div class="product">
-                    <img src="assets/img/blue-shirt.png" alt="blue shirt">
-                    <h2>Blue Shirt</h2>
-                    <p>$20</p>
-                    <a href="#">Buy</a>
-                </div>
-
-                <div class="product">
-                    <img src="assets/img/red-shirt.png" alt="blue shirt">
-                    <h2>Red Shirt</h2>
-                    <p>$20</p>
-                    <a href="#">Buy</a>
-                </div>
-
-                <div class="product">
-                    <img src="assets/img/green-shirt.png" alt="blue shirt">
-                    <h2>Green Shirt</h2>
-                    <p>$20</p>
-                    <a href="#">Buy</a>
-                </div>
-
-            </div>
-            <!-- /. main -->
-        </div>
-
-        <!-- footer -->
-        <footer id="footer">
-            <p>Developed by César Obed Figueroa Luna &copy; <?=date('2023')?></p>
-        </footer>
-        <!-- /. footer -->
-
+            <!-- footer -->
+            <footer id="footer">
+                <p>Developed by César Obed Figueroa Luna &copy; <?=date('2023')?></p>
+            </footer>
+            <!-- /. footer -->
+        </div> <!-- /. container -->
         
     </body>
 
