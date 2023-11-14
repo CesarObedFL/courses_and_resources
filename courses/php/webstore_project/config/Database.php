d33924bae084cd71b1f2bbb7f0e3dd7fd5ec8a28
CesarObedFL <cesarobedfl@gmail.com>
2023-11-14 13:18:21 -0600

    [ADD] webstore_project; database class

---
+++ courses/php/webstore_project/config/Database.php
@@ -0,0 +1,19 @@
+<?php
+
+class Database {
+
+    private static $server = 'cyr_database_container';
+    private static $username = 'root'; // using root for user permissions
+    private static $password = 'secret';
+    private static $database = 'webstore_db';
+
+    public static function connect()
+    {
+        $connection = new mysqli(self::$server, self::$username, self::$password, self::$database);
+
+        $connection->query("SET NAMES 'utf8'");
+
+        return $connection;
+    }
+
+}
