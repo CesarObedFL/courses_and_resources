931240c4c1571a9e616cbff7be958007775c90bf
CesarObedFL <cesarobedfl@gmail.com>
2023-11-17 12:01:25 -0600

    [ADD] webstore_project; project default parameters

---
+++ courses/php/webstore_project/config/Parameters.php
@@ -0,0 +1,6 @@
+<?php
+
+// defualt project parameters
+define ("base_url", $_SERVER['DOCUMENT_ROOT']."/courses_and_resources/courses/php/webstore_project/");
+define ("default_controller", "ProductController");
+define ("default_action", "index");
