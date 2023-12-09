1d9ad769afacb26422e30e2ca96d756d9f1d2bba
CesarObedFL <cesarobedfl@gmail.com>
2023-12-08 20:41:02 -0600

    [ADD] webstore_project; show product styles

--- courses/php/webstore_project/views/products/show.php
+++ courses/php/webstore_project/views/products/show.php
@@ -1,16 +1,20 @@
 
 <?php if( isset($product) ): ?>
     <h2><?=$product->name?></h2>
-    <div class="product">
-        <?php if($product->image != null): ?>
-            <img src="assets/img/products/<?=$product->image?>" alt="<?=$product->image?>" width="50" height="100">
-        <?php else: ?>
-            <img src="assets/img/products/sample-product.png" alt="product" width="50" height="100">
-        <?php endif; ?>
-        <h2><?=$product->name?></h2>
-        <p>$<?=$product->price?></p>
-        <a href="#" class="buy-button">Buy</a>
-        <a href="#" class="buy-button">Add to Cart</a>
+    <div class="detail-product">
+        <div class="detail-product-img">
+            <?php if($product->image != null): ?>
+                <img src="assets/img/products/<?=$product->image?>" alt="<?=$product->image?>" width="50" height="100">
+            <?php else: ?>
+                <img src="assets/img/products/sample-product.png" alt="product" width="50" height="100">
+            <?php endif; ?>
+        </div>
+        <div class="detail-product-data">
+            <h2><?=$product->name?></h2>
+            <p>$<?=$product->price?></p>
+            <a href="#" class="buy-button">Buy</a>
+            <a href="#" class="buy-button">Add to Cart</a>
+        </div>
     </div>
 <?php else: ?>
     <div class="alert alert-error">
