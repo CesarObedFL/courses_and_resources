b7b309943155e727de051e1dbbaf8d0a45595247
CesarObedFL <cesarobedfl@gmail.com>
2023-12-18 16:52:42 -0600

    [FIX] webstore_project; order_details

--- courses/php/webstore_project/views/orders/order_detail.php
+++ courses/php/webstore_project/views/orders/order_detail.php
@@ -1,4 +1,4 @@
-<h2>Order <?=$order->id?></h2>
+<h2>Order <?=($order->id) ?? ''?></h2>
 
 <?php if ( isset($_SESSION['msg']) ):?>
     <div class="alert alert-<?=$_SESSION['status']?>">
@@ -7,36 +7,57 @@
     </div>
 <?php endif;?>
 
+<?php if( isset($order) && isset($products) ): ?>
 
-<table>
-    <thead>
-        <tr>
-            <th>Product</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Units</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php while( $product = $products->fetch_object() ):?>
+    <dl>
+        <dt><strong>Datetime</strong></dt>
+        <dd><?=$order->datetime?></dd>
+        <br>
+
+        <dt><strong>Information</strong></dt>
+        <dd><?='Total: $'.$order->total?></dd>
+        <dd><?='Amount: '.$order->amount?></dd>
+        <br>
+
+        <dt><strong>Shipment Address</strong></dt>
+        <dd><?=$order->address . " " . $order->state . " " . $order->country?></dd>
+    </dl>
+
+    <table>
+        <thead>
             <tr>
-                <td>
-                    <a href="index.php?controller=Product&action=show&id=<?=$product->id?>">
-                        <?php if($product->image != null): ?>
-                            <img src="assets/img/products/<?=$product->image?>" alt="<?=$product->image?>" width="50" height="100">
-                        <?php else: ?>
-                            <img src="assets/img/products/sample-product.png" alt="product" width="50" height="100">
-                        <?php endif; ?>
-                    </a>
-                </td>
-                <td><?=$product->name?></td>
-                <td><?='$'.$product->price?></td>
-                <td><?=$product->units?></td>
+                <th>Product</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Units</th>
             </tr>
-        <?php endwhile;?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php while( $product = $products->fetch_object() ):?>
+                <tr>
+                    <td>
+                        <a href="index.php?controller=Product&action=show&id=<?=$product->id?>">
+                            <?php if($product->image != null): ?>
+                                <img src="assets/img/products/<?=$product->image?>" alt="<?=$product->image?>" width="50" height="100">
+                            <?php else: ?>
+                                <img src="assets/img/products/sample-product.png" alt="product" width="50" height="100">
+                            <?php endif; ?>
+                        </a>
+                    </td>
+                    <td><?=$product->name?></td>
+                    <td><?='$'.$product->price?></td>
+                    <td><?=$product->units?></td>
+                </tr>
+            <?php endwhile;?>
+        </tbody>
+    </table>
 
+<?php else: ?>
+    <div class="alert alert-error">
+        <strong>Something went wrong!...</strong>
+        <span class="close-alert-btn" onclick="this.parentElement.style.display='none';">&times;</span>
+    </div>
 
+<?php endif;?>
 
 <?php Utils::delete_alert_messages(); ?>
