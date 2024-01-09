e29e84747a902e65e189cbc56d105d4b25ddd9b4
CesarObedFL <cesarobedfl@gmail.com>
2024-01-09 08:38:48 -0600

    [ADD] resources; css play_button: rainbow play button script

--- resources/css/play_buttons/rainbow_play_button/js/script.js
+++ resources/css/play_buttons/rainbow_play_button/js/script.js
@@ -1,3 +1,23 @@
-(function() {
+var pause = true;
 
-})();
+var icon = document.getElementById('icon');
+
+document.getElementById('button').addEventListener('click', function () {
+    
+    if (pause == true) {
+        $('head').append("<style>.gradient-border::after{ animation-duration: 1.4s }</style>");
+        icon.style.transform = 'rotate(0deg)';
+        icon.style.borderLeft = '6px solid white';
+        icon.style.borderBottom = '6px solid #2c2f30';
+        document.getElementById('music').play();
+        pause = false;
+    }
+    else {
+        $('head').append("<style>.gradient-border::after{ animation-duration: 3s }</style>");
+        icon.style.transform = 'rotate(-45deg)';
+        icon.style.borderLeft = '6px solid #2c2f30';
+        icon.style.borderBottom = '6px solid white';
+        document.getElementById('music').pause();
+        pause = true;
+    }
+});
