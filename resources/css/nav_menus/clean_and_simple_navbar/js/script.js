e43c565ec3757df6b6ae6b603b172b4f47ac38e8
CesarObedFL <cesarobedfl@gmail.com>
2024-01-17 10:37:05 -0600

    [FIX] resources; css nav menus: clean & simple navbar

--- resources/css/nav_menus/clean_and_simple_navbar/js/script.js
+++ resources/css/nav_menus/clean_and_simple_navbar/js/script.js
@@ -1,24 +1,21 @@
-let menuIcon = document.querySelector('.menuIcon');
-let nav = document.querySelector('.overlay-menu');
+$(document).ready(function() {
+    let menuIcon = $('.menuIcon');
+    let nav = $('.overlay-menu');
 
-menuIcon.addEventListener('click', () => {
-    if (nav.style.transform != 'translateX(0%)') {
-        nav.style.transform = 'translateX(0%)';
-        nav.style.transition = 'transform 0.2s ease-out';
-    } else {
-        nav.style.transform = 'translateX(-100%)';
-        nav.style.transition = 'transform 0.2s ease-out';
-    }
+    menuIcon.click(function() {
+        if ( !menuIcon.hasClass('toggle') ) {
+            nav.css({
+                'transform': 'translateX(0%)',
+                'transition': 'transform 0.2s ease-out'
+            });
+            menuIcon.addClass('toggle');
+        } else {
+            nav.css({
+                'transform': 'translateX(-100%)',
+                'transition': 'transform 0.2s ease-out'
+            });
+            menuIcon.removeClass('toggle');
+        }
+    });
 });
 
-
-// Toggle Menu Icon ========================================
-let toggleIcon = document.querySelector('.menuIcon');
-
-toggleIcon.addEventListener('click', () => {
-    if (toggleIcon.className != 'menuIcon toggle') {
-        toggleIcon.className += ' toggle';
-    } else {
-        toggleIcon.className = 'menuIcon';
-    }
-});
