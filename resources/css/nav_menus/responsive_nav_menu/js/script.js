0bc8c500e55bbfc5dc1dd366659a876f575c86de
CesarObedFL <cesarobedfl@gmail.com>
2024-01-30 10:55:00 -0600

    [ADD] resources; css nav_menus: responsive_nav_menu scripts

--- resources/css/nav_menus/responsive_nav_menu/js/script.js
+++ resources/css/nav_menus/responsive_nav_menu/js/script.js
@@ -1,3 +1,66 @@
-(function() {
+jQuery.noConflict();
+jQuery(document).ready(function ($) {
+    $('.abrir_menu').click(function () {
+        $("body").toggleClass('no_scroll');
+    });
+});
 
-})();
+jQuery.noConflict();
+jQuery(document).ready(function ($) {
+    $(function () {
+        initDropDowns($("#menu"));
+    });
+
+    function initDropDowns(allMenus) {
+        allMenus.children(".abrir_menu").on("click", function () {
+            var thisTrigger = jQuery(this),
+                thisMenu = thisTrigger.parent(),
+                thisPanel = thisTrigger.next();
+
+            if (thisMenu.hasClass("open")) {
+                thisMenu.removeClass("open");
+                jQuery(document).off("click");
+                thisPanel.off("click");
+            }
+            else {
+                allMenus.removeClass("open");
+                thisMenu.addClass("open");
+                jQuery(document).on("click", function () {
+                    allMenus.removeClass("open");
+                });
+                thisPanel.on("click", function (e) {
+                    e.stopPropagation();
+                });
+            }
+            return false;
+        });
+    }
+});
+
+jQuery(document).ready(function ($) {
+    $(".menu_aberto").on("click", function () {
+
+        if ($(this).has) {
+            $("body").removeClass("no_scroll");
+        }
+    });
+});
+
+jQuery.noConflict();
+jQuery(document).ready(function ($) {
+    $(document).ready(function () {
+        $(".nav li a").each(function () {
+            if ($(this).next().length > 0) {
+                $(this).addClass("parent");
+            };
+        })
+    })
+
+    $(function () {
+        $(".nav li").unbind('mouseenter mouseleave');
+        $(".nav li a.parent").unbind('click').bind('click', function (e) {
+            $(this).parent("li").toggleClass("hover");
+            e.preventDefault();
+        });
+    });
+});
