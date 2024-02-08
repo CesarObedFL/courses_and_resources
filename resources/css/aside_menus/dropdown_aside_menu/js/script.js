755ac6f3afb74124493d48d87f98fa86d407751b
CesarObedFL <cesarobedfl@gmail.com>
2024-02-08 11:25:27 -0600

    [ADD] resources; css aside_menus: dropdown_aside_menu script

--- resources/css/aside_menus/dropdown_aside_menu/js/script.js
+++ resources/css/aside_menus/dropdown_aside_menu/js/script.js
@@ -1,3 +1,33 @@
-(function() {
+document.addEventListener('DOMContentLoaded', function () {
+    // Toggle Switch
+    const container = document.querySelector('.container')
+    const toggleSwitch = document.querySelector('.switch')
 
-})();
+    toggleSwitch.addEventListener('click', () => {
+        container.classList.toggle('dark-theme')
+    })
+
+    const aside = document.querySelector('aside')
+    const toggleMenu = document.querySelector('.toggle-menu')
+
+    toggleMenu.addEventListener('click', () => {
+        aside.classList.toggle('active')
+        toggleMenu.classList.toggle('active')
+    })
+
+
+    // Dropdown Menu
+    const mainMenus = document.querySelectorAll('.main-menu')
+
+    mainMenus.forEach((mainMenu) => {
+        const subMenu = mainMenu.nextElementSibling
+
+        if (subMenu) {
+            mainMenu.addEventListener('click', () => {
+                mainMenu.classList.toggle('active')
+                subMenu.classList.toggle('active')
+            })
+        }
+    })
+
+})
