73bb48f2a47fcb39f118a47d68d3ee6f1c14dfe5
CesarObedFL <cesarobedfl@gmail.com>
2024-02-12 08:26:57 -0600

    [ADD] resources; jquery: randomString.js

---
+++ resources/jquery/randomString.js
@@ -0,0 +1,13 @@
+function randomString(string_lenght) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789';
+
+    let characters_lenght = characters.length;
+
+    for ( var i = 0; i < string_lenght; i++ ) {
+        result += characters.charAt( Math.floor( Math.random() * characters_lenght ) );
+    }
+
+    return result;
+
+}
