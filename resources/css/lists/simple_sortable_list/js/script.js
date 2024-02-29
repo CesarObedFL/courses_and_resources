f8d73c497d53520da64ee7607a3e547ec1e0771a
CesarObedFL <cesarobedfl@gmail.com>
2024-02-29 11:00:29 -0600

    [ADD] resources; lists: simple_sortable_list script

--- resources/css/lists/simple_sortable_list/js/script.js
+++ resources/css/lists/simple_sortable_list/js/script.js
@@ -1,3 +1,13 @@
-(function() {
+$('.sortable-list').sortable({
+    connectWith: '.sortable-list',
+    update: function (event, ui) {
+        var changedList = this.id;
+        var order = $(this).sortable('toArray');
+        var positions = order.join(';');
 
-})();
+        console.log({
+            id: changedList,
+            positions: positions
+        });
+    }
+});
