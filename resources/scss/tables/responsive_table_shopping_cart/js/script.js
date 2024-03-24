922b6f0ecf49ec6b371636dd472bfe2236bac54e
CesarObedFL <cesarobedfl@gmail.com>
2024-03-24 13:16:08 -0600

    [ADD] resources; scss tables: responsive_table_shopping_cart script

--- resources/scss/tables/responsive_table_shopping_cart/js/script.js
+++ resources/scss/tables/responsive_table_shopping_cart/js/script.js
@@ -1,3 +1,12 @@
-(function() {
+// Remove Items From Cart
+$('a.remove').click(function (e) {
+    e.preventDefault();
+    $(this).parent().parent().parent().hide(400);
 
-})();
+})
+
+// Just for testing, show all items
+$('a.btn.continue').click(function () {
+    $('li.items').show(400);
+})
+
