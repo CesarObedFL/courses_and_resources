6db9a988f61b50b36408fa0d0cf4edc9c7728add
CesarObedFL <cesarobedfl@gmail.com>
2024-03-29 10:59:29 -0600

    [ADD] resources; scss alerts: e2e_alert_messages script

--- resources/scss/alerts/e2e_allert_messages/js/script.js
+++ resources/scss/alerts/e2e_allert_messages/js/script.js
@@ -1,3 +1,10 @@
-(function() {
+var closesIcon = document.querySelectorAll('.xd-message .close-icon');
 
-})();
+closesIcon.forEach(function (closeIcon) {
+    closeIcon.addEventListener('click', function () {
+        this.parentNode.parentNode.classList.add('hide');
+    });
+});
+
+
+
