96a40ddfbfe1f8597bf52ccca4e2b92837f9148f
CesarObedFL <cesarobedfl@gmail.com>
2024-05-07 08:54:02 -0600

    [ADD] resources; scss tabs: simple_tabs scripts

--- resources/scss/tabs/simple_tabs/js/script.js
+++ resources/scss/tabs/simple_tabs/js/script.js
@@ -1,3 +1,28 @@
-(function() {
 
-})();
+var tab = $(".tab"),
+    menus = $(".menu li"),
+    contents = $(".content>div"),
+    cur = 0,
+    timer = null;
+
+menus.on("click", function () {
+    var _this = $(this);
+    var thisIndex = _this.index();
+    cur = thisIndex;
+
+    _this.addClass("active").siblings().removeClass("active");
+    contents.eq(thisIndex).stop(true).slideDown().siblings().stop(true).slideUp();
+
+});
+
+
+function autoPlay() {
+    timer = setInterval(function () {
+        if (cur > menus.length - 1) {
+            cur = 0;
+        }
+        menus.eq(cur).trigger("click");
+        cur++;
+    }, 2000);
+}
+
