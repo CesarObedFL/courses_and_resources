423734591703fe3423b04fbd571c77b4741b5daf
CesarObedFL <cesarobedfl@gmail.com>
2024-05-09 09:51:48 -0600

    [ADD] resources; scss aside_menus: notebook_aside_menu scripts

--- resources/scss/aside_menus/notebook_aside_menu/js/script.js
+++ resources/scss/aside_menus/notebook_aside_menu/js/script.js
@@ -1,3 +1,23 @@
-(function() {
+$(function () {
 
-})();
+    // menu items
+    var menuItems = [{ caption: 'Automobile', icon: 'auto' }, { caption: 'Cafe', icon: 'cafe' }, { caption: 'Construction', icon: 'construction' }, { caption: 'Education', icon: 'education' },
+    { caption: 'Fainance', icon: 'fainance' },
+    { caption: 'Food Court', icon: 'food' }, { caption: 'Furniture', icon: 'furniture' }, { caption: 'Home & Garden', icon: 'home' },
+    { caption: 'Medical', icon: 'medical' },
+    { caption: 'Sport & Outdoor', icon: 'sport' }, { caption: 'Technologies', icon: 'technology' }, { caption: 'Travel', icon: 'travel' },
+    { caption: 'Wedding', icon: 'wedding' }];
+
+    var viewModel = function () {
+        this.menuItems = ko.observableArray(menuItems);
+    }
+
+    ko.bindingHandlers.cssIcon = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            element.className = valueAccessor() + ' ' + element.className;
+
+        }
+    };
+
+    ko.applyBindings(new viewModel());
+});
