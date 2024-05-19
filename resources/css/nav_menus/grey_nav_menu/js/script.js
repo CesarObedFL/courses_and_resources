6102cc5e7ade382205fd81a225a517007b1b21f6
CesarObedFL <cesarobedfl@gmail.com>
2024-05-18 19:13:43 -0600

    [ADD] resources; css nav_menus; grey_nav_menu script

--- resources/css/nav_menus/grey_nav_menu/js/script.js
+++ resources/css/nav_menus/grey_nav_menu/js/script.js
@@ -1,3 +1,13 @@
-(function() {
+$(document).ready(function () {
 
-})();
+    $('.nav-container').click(function () {
+        if ($('aside').hasClass('')) {
+            $('aside').addClass('is-clicked');
+            $('.nav-container').addClass('nav-clicked');
+        } else {
+            $('aside').removeClass('is-clicked');
+            $('.nav-container').removeClass('nav-clicked');
+        }
+    });
+
+})
