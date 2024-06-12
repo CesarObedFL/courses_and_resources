8a3e7d201c09b7a3bd6e27acf29f62e4a1f98297
CesarObedFL <cesarobedfl@gmail.com>
2024-06-12 08:36:12 -0600

    [ADD] resources; scss layouts: sticky_sidebar_navigation_concept_layout script

--- resources/scss/layouts/sticky_sidebar_navigation_concept_layout/js/script.js
+++ resources/scss/layouts/sticky_sidebar_navigation_concept_layout/js/script.js
@@ -1,3 +1,18 @@
-(function() {
+window.addEventListener("scroll", doPinnedCheck);
 
-})();
+function doPinnedCheck() {
+    const stations = document.querySelectorAll(".line .station");
+
+    const pinnedStations = [...stations].filter((station) => {
+        return station.getBoundingClientRect().top <= 193;
+    }).slice(1);
+
+    if (!pinnedStations) {
+        return;
+    }
+
+    document.documentElement.style.setProperty(
+        "--tfl-station-index",
+        pinnedStations.length
+    );
+}
