217c7efab9be0f810eab1d2e382a4e74b39b2e3b
CesarObedFL <cesarobedfl@gmail.com>
2024-06-13 09:02:23 -0600

    [ADD] resources; scss layouts: navigation_sticky_with_burger_expandible_layout script

--- resources/scss/layouts/navigation_sticky_with_burger_expandable_layout/js/script.js
+++ resources/scss/layouts/navigation_sticky_with_burger_expandable_layout/js/script.js
@@ -1,3 +1,18 @@
-(function() {
+jQuery(document).ready(function () {
+    const mainNav = $('#mainNav');
+    const burger = $('#mainNavBurger');
+    const link = $('.nav-content span');
 
-})();
+    burger.on('click', function () {
+        clickMainNav();
+    });
+    link.on('click', function () {
+        clickMainNav();
+    });
+
+    function clickMainNav() {
+        burger.toggleClass('burger-open')
+        mainNav.toggleClass('main-nav-open');
+    }
+
+});
