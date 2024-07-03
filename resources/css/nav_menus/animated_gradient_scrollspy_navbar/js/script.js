10aeeac41a35bb2c470fb8a8d00bf2b07865287d
CesarObedFL <cesarobedfl@gmail.com>
2024-07-03 08:43:03 -0600

    [ADD] resources; css nav_menus: animated_gradient_scrollspy_navbar script

--- resources/css/nav_menus/animated_gradient_scrollspy_navbar/js/script.js
+++ resources/css/nav_menus/animated_gradient_scrollspy_navbar/js/script.js
@@ -1,3 +1,23 @@
-(function() {
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
 
-})();
+    function highlightNavLink() {
+        const scrollPosition = window.scrollY;
+
+        navLinks.forEach(link => {
+            const sectionId = link.getAttribute("href").substring(1); // Rimuovi il carattere #
+            const section = document.getElementById(sectionId);
+
+            if (section.offsetTop <= scrollPosition && section.offsetTop + section.offsetHeight > scrollPosition) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", highlightNavLink);
+
+    // Chiamare la funzione per inizializzare lo stato all'avvio della pagina
+    highlightNavLink();
+});
