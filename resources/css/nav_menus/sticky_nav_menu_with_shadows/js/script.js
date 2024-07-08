7bb84ee1703d93fe3ec646bc9fd301ba43f7e2e9
CesarObedFL <cesarobedfl@gmail.com>
2024-07-07 18:36:57 -0600

    [ADD] resources; css nav_menus: sticky_nav_menu_with_shadows script

--- resources/css/nav_menus/sticky_nav_menu_with_shadows/js/script.js
+++ resources/css/nav_menus/sticky_nav_menu_with_shadows/js/script.js
@@ -1,3 +1,16 @@
-(function() {
+document.addEventListener("DOMContentLoaded", () => {
 
-})();
+    let activate = document.querySelectorAll('nav a');
+
+
+    function activateLink() {
+        let active = document.querySelector('.active');
+        active.classList.remove('active');
+        this.classList.add('active');
+    };
+
+    activate.forEach((item) =>
+        item.addEventListener('click', activateLink)
+    );
+
+});
