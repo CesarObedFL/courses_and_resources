f5b08d655766266da700ddd51ffb3ef9caa69c65
CesarObedFL <cesarobedfl@gmail.com>
2024-07-14 16:16:00 -0600

    [ADD] resources; css nav_menus: fixed_galaxy_nav_menu scripts

--- resources/css/nav_menus/fixed_galaxy_nav_menu/js/script.js
+++ resources/css/nav_menus/fixed_galaxy_nav_menu/js/script.js
@@ -1,3 +1,18 @@
-(function() {
+const nav = document.getElementById('nav');
+let item = document.getElementById('item-earth');
+if(item){
+	item.style.border = "1px solid white"
+}
 
-})();
+nav.addEventListener('click',(e)=>{
+	
+	const selectedItem = e.target.href.split("#")[1]
+
+	if(item && item.id.split("-")[1] !== selectedItem.toLowerCase()){
+		console.log(item.id.split("-")[1], selectedItem.toLowerCase())
+		item.style.border= 'none'
+	}
+	
+	item = document.getElementById(`item-${selectedItem.toLowerCase()}`)
+	item.style.border="1px solid white"
+})
