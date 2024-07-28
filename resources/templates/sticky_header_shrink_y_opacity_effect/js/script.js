bd3a09e4c46141aab601d44b02603069f6f4eb4d
CesarObedFL <cesarobedfl@gmail.com>
2024-07-27 20:25:06 -0600

    [ADD] resources; templates: sticky_header_shrink_y_opacity_effect script

--- resources/templates/sticky_header_shrink_y_opacity_effect/js/script.js
+++ resources/templates/sticky_header_shrink_y_opacity_effect/js/script.js
@@ -1,3 +1,8 @@
-(function() {
-
-})();
+window.onscroll = function () {
+    var header = document.querySelector("header");
+    if (window.pageYOffset > 0) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+};
