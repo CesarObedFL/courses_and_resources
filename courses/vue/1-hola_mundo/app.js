08f14cadffbb79941be094cda37478f84ee0be44
CesarObedFL <cesarobedfl@gmail.com>
2024-08-06 21:05:31 -0600

    [ADD] courses; vue: 1-hola_mundo reactive variables

--- courses/vue/1-hola_mundo/app.js
+++ courses/vue/1-hola_mundo/app.js
@@ -2,9 +2,24 @@
 
     const app = createApp ({
         template: `
-            <h1>Hola mundo</h1>
-            <p>Desde Vue</p>
-        `
+            <h1>{{ welcome_message }}</h1>
+            <p>{{ name }}</p>
+        `,
+
+        setup() {
+            const welcome_message = ref('Hola mundo');
+            const name = ref('desde vue');
+
+            setTimeout( () => {
+                name.value = "soy césar obed figueroa luna";
+            }, 1000);
+
+            return {
+                welcome_message,
+                name,
+            }
+        }
+
     });
 
     app.mount('#myApp');
