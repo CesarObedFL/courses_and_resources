1f55bac3fb531f0058a92840e86a70df4c92dc0d
CesarObedFL <cesarobedfl@gmail.com>
2024-08-09 12:24:37 -0600

    [ADD] courses; typescript: 01-const-let docs

--- courses/typescript/src/bases/01-const-let.ts
+++ courses/typescript/src/bases/01-const-let.ts
@@ -1,7 +1,10 @@
 
-
+// consts variables do not change 
 const first_name = 'César Obed';
 const last_name = 'Figueroa Luna';
+
+// let variables change during the program
+let email_address = 'cesarobedfl@gmail.com';
 
 export const full_name = `${first_name} ${last_name}`;
 
