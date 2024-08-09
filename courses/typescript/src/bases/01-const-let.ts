bab300862bdbbc30cb6dfde7591f1b8ea8f978e2
CesarObedFL <cesarobedfl@gmail.com>
2024-08-09 12:20:40 -0600

    [ADD] courses; typescript: consts and let variables

---
+++ courses/typescript/src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+
+
+const first_name = 'César Obed';
+const last_name = 'Figueroa Luna';
+
+export const full_name = `${first_name} ${last_name}`;
+
+//console.log({first_name, last_name});
+
+//console.log({full_name});
