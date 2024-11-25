6e7343709f09d43fb81b0f0f9402e16926c8eed4
CesarObedFL <cesarobedfl@gmail.com>
2024-11-24 18:30:16 -0600

    [ADD] resources; css aside_menus: glassmorphic_side_navigation_menu script

--- resources/css/aside_menus/glassmorphic_side_navigation_menu/js/script.js
+++ resources/css/aside_menus/glassmorphic_side_navigation_menu/js/script.js
@@ -1,3 +1,28 @@
-(function() {
+const list = document.querySelectorAll('nav ul li');
+const menuToggle = document.querySelector('.toggle');
+const nav = document.querySelector('nav');
+const pageMask = document.querySelector('#page-mask')
+const body = document.body;
 
-})();
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+    if (!nav.classList.contains('active')) {
+        body.classList.add('nav-active');
+        pageMask.style.display = 'block';
+    }
+    else {
+        body.classList.remove('nav-active');
+    }
+})
+
+for (let item of list) {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+        let j = 0;
+        for (let listItem of list) {
+            listItem.classList.remove('active');
+        }
+        this.classList.add('active');
+    })
+}
