253e5462858c0c7874861ced3ef41db417daa027
CesarObedFL <cesarobedfl@gmail.com>
2025-05-28 05:02:49 -0600

    [ADD] resources; css notifications: notification_rainbow scripts

--- resources/css/notifications/notification_rainbow/js/script.js
+++ resources/css/notifications/notification_rainbow/js/script.js
@@ -1,3 +1,26 @@
-(function() {
+const notificationCount = document.querySelector(".notification-count");
+const notificationBell = document.querySelector(".notification-bell");
+let count = 0;
 
-})();
+const random = (min, max) => min + Math.random() * (max - min);
+
+function newNotification() {
+    count++;
+
+    notificationBell.classList.add("animate");
+    setTimeout(() => notificationBell.classList.remove("animate"), 1500);
+    setTimeout(newNotification, random(1500, 2700));
+
+    if (count === 1) {
+        notificationCount.style.visibility = "visible";
+    }
+
+    if (count > 9) {
+        notificationCount.textContent = "9+";
+        return;
+    }
+
+    notificationCount.textContent = count;
+}
+
+setTimeout(newNotification, 1200);
