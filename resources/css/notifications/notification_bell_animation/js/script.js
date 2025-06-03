5e9e5c6c5921f625f08d7f2ea5db954e5d58f4fc
CesarObedFL <cesarobedfl@gmail.com>
2025-06-03 05:42:53 -0600

    [ADD] resources; css notifications: notification_bell_animation script

--- resources/css/notifications/notification_bell_animation/js/script.js
+++ resources/css/notifications/notification_bell_animation/js/script.js
@@ -1,3 +1,18 @@
-(function() {
+const buttonElement = document.querySelector(".notification-bell");
+const countElement = document.getElementById("count");
+const firstNotificationElement = document.getElementById("first");
+const secondNotificationElement = document.getElementById("second");
+const thirdNotificationElement = document.getElementById("third");
 
-})();
+let count = 3;
+
+setInterval(function () {
+    count += 1;
+    countElement.innerText = count;
+}, 2000);
+
+for (let i = 0; i < 11; i++) {
+    firstNotificationElement.classList.add("notify-first");
+    secondNotificationElement.classList.add("notify-second");
+    thirdNotificationElement.classList.add("notify-third");
+}
