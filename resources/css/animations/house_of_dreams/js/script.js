473b6a4f5c1682235a3c80099ae87c17b54b031a
CesarObedFL <cesarobedfl@gmail.com>
2026-02-16 05:50:54 -0600

    [FIX] resources; css animations: house_of_dreams script

--- resources/css/animations/house_of_dreams/js/script.js
+++ resources/css/animations/house_of_dreams/js/script.js
@@ -1,3 +1,14 @@
-(function() {
+// in my house
+// of dreams
+// it rains happiness
+// in all the colours
 
-})();
+/* It looks and sounds much better in French though */
+
+gsap.from("svg circle", {
+    cx: "67%",
+    cy: "63%",
+    duration: 3,
+    ease: "circ.out",
+    stagger: 0.05
+});
